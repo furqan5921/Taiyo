3b1fc1f0fb2338256d2ac91b147cf043de561280
Mohammad <furqanali5921@gmail.com>
2023-04-25 23:41:57 +0530

    done project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-
-import './App.css';
+import Sidebar from "./component/Sidebar";
+import "./index.css";
+import { useState } from "react";
+import Contact from "./pages/Contact";
+import GraphAndChart from "./pages/GraphAndChart";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
+  const [currentView, setCurrentView] = useState<string>("contact");
+  const handleView = (view: string) => {
+    setCurrentView(view);
+  };
+
   return (
-    <div className="App">
-     
+    <div className="flex w-full">
+      <Sidebar view={currentView} changeView={handleView} />
+      <AllRoutes />
     </div>
   );
 }
